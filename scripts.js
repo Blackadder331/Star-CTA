6cbb7ca72a542c3096850f145aaa82f69fe02e03
R Sharp <reynolds.sharp@gmail.com>
2023-04-27 09:49:23 -0500

    starburt sticker CTA

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// A button shaped as a star with 20 points that morphs to a circle on hover
+//a 20 sided star shaped button that morphs into a circle CTA when hovered
+
+// document
+//   .getElementById("morphing-button")
+//   .addEventListener("mouseover", function () {
+//     document
+//       .getElementById("star-shape")
+//       .setAttribute(
+//         "points",
+//         "50,50 50,50 50,50 50,50 50,50 50,50 50,50 50,50 50,50 50,50"
+//       );
+//   });
+
+// document
+//   .getElementById("morphing-button")
+//   .addEventListener("mouseout", function () {
+//     document
+//       .getElementById("star-shape")
+//       .setAttribute(
+//         "points",
+//         "50,1 61,34 95,34 68,53 78,87 50,70 22,87 32,53 5,34 39,34"
+//       );
+//   });
